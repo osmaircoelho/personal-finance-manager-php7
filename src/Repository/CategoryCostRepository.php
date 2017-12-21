cd1ba29321a21504fe9ffbb0ce2a80a13342a80b
Osmair Coelho <osmair.coelho@gmail.com>
2017-12-21 15:50:50 +0000

    testing extract, counting expenses in cost categories, creating expense graphs, ending user menu

--- src/Repository/CategoryCostRepository.php
+++ src/Repository/CategoryCostRepository.php
@@ -21,6 +21,7 @@ class CategoryCostRepository extends DefaultRepository implements CategoryCostRe
             ->selectRaw('category_costs.name, sum(value) as value')
             ->leftJoin('bill_pays', 'bill_pays.category_cost_id', '=', 'category_costs.id')
             ->where('category_costs.user_id', $userId)
+            ->whereNotNull('bill_pays.category_cost_id')
             ->whereBetween('date_launch', [$dateStart, $dateEnd])
             ->groupBy('value')
             ->groupBy('category_costs.name')
