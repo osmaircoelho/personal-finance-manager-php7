016708a2725e37ddf70f32961f3991d9f14ffc1d
Osmair Coelho <osmair.coelho@gmail.com>
2019-01-14 20:38:57 +0000

    correcting error in query

--- src/Repository/CategoryCostRepository.php
+++ src/Repository/CategoryCostRepository.php
@@ -4,29 +4,26 @@ namespace SONFin\Repository;
 
 use SONFin\Models\CategoryCost;
 
-class CategoryCostRepository extends DefaultRepository implements CategoryCostRepositoryInterface
-{
+class CategoryCostRepository extends DefaultRepository implements CategoryCostRepositoryInterface {
 
+	/**
+	 * CategoryCostRepository constructor.
+	 */
+	public function __construct() {
+		parent::__construct( CategoryCost::class );
+	}
 
-    /**
-     * CategoryCostRepository constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct(CategoryCost::class);
-    }
+	public function sumByPeriod( string $dateStart, string $dateEnd, int $userId ): array {
+		$categories = CategoryCost::query()
+		                          ->selectRaw( 'category_costs.name, sum(value) as value' )
+		                          ->leftJoin( 'bill_pays', 'bill_pays.category_cost_id', '=', 'category_costs.id' )
+		                          ->whereBetween( 'date_launch', [ $dateStart, $dateEnd ] )
+		                          ->where( 'category_costs.user_id', $userId )
+		                          ->whereNotNull( 'bill_pays.category_cost_id' )
+		                          ->groupBy( 'category_costs.name' )
+		                          ->get();
 
-    public function sumByPeriod(string $dateStart, string $dateEnd, int $userId): array
-    {
-        $categories = CategoryCost::query()
-            ->selectRaw('category_costs.name, sum(value) as value')
-            ->leftJoin('bill_pays', 'bill_pays.category_cost_id', '=', 'category_costs.id')
-            ->where('category_costs.user_id', $userId)
-            ->whereNotNull('bill_pays.category_cost_id')
-            ->whereBetween('date_launch', [$dateStart, $dateEnd])
-            ->groupBy('value')
-            ->groupBy('category_costs.name')
-            ->get();
-        return $categories->toArray();
-    }
+		return $categories->toArray();
+
+	}
 }
